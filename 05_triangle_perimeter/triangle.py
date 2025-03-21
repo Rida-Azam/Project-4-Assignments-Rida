3d3cb028c101eeef42736ba4a39a0385f341f3e3
Rida <samiaazam50@gmail.com>
2025-03-22 02:19:46 +0500

    done

---
+++ 05_triangle_perimeter/triangle.py
@@ -0,0 +1,8 @@
+def main():
+    side1:float=float(input("What is the length of side 1?"))
+    side2:float=float(input("What is the length of side 2?"))
+    side3:float=float(input("What is the length of side 3?"))
+
+    print(f"The perimeter of the trinagle is {side1+side2+side3}")
+
+main()    
---
+++ 06_square_number/square.py
@@ -0,0 +1,5 @@
+def main():
+    num:float=float(input("Type a number to see its square :"))
+    print(f"{num} squared is {num*num}")
+
+main()    
