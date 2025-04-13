9d37b6e8cced966ca06cd8a5e54894b3fb3cbf1d
Rida-Azam <125395525+Rida-Azam@users.noreply.github.com>
2025-04-14 01:03:00 +0500

    Delete 03_fahrenheit_to_celsius directory

--- 03_fahrenheit_to_celsius/fah_to_cel.py
+++
@@ -1,7 +0,0 @@
-def main():
-    fahrenheit=int(input("Enter temperture in fahrenheit :"))
-    degrees_celsius = (fahrenheit - 32) * 5.0/9.0
-    print(f"Temperatue:{fahrenheit}F = {degrees_celsius}C")
-
-main()    
-
